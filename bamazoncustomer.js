d03e997c0bcf8fedbd8fe587def9e77100c152d5
Jebbla <jlp2722@gmail.com>
2019-07-05 10:53:50 -0500

    added package.json

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,11 +1,15 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+ 
  var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "Pickle$1",
+        password: "",
         database: "bamazon"
      });
      connection.connect(function (err) {
         if (err) throw err;
-        console.log("Hi");
+       console.log("Hi");
         });
